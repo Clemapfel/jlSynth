210472ed52672f681d55850fe01cfb66214fd869
Clemapfel <mail@clemens-cords.com>
2022-11-06 19:20:12 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,51 +1,108 @@
-//
-// Created by clem on 6/25/22.
-//
+#include <soundio/soundio.h>
 
-#include <cmath>
-#include <cassert>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-#include <SFML/Audio.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-
-#include <SDL2/SDL_mixer.h>
-
-#include <chrono>
-#include <thread>
-
-#include <iostream>
-#include <cmath>
-
-int main()
+static const float PI = 3.1415926535f;
+static float seconds_offset = 0.0f;
+static void write_callback(struct SoundIoOutStream *outstream,
+                           int frame_count_min, int frame_count_max)
 {
-    const size_t sample_rate = 44100;
+    const struct SoundIoChannelLayout *layout = &outstream->layout;
+    float float_sample_rate = outstream->sample_rate;
+    float seconds_per_frame = 1.0f / float_sample_rate;
+    struct SoundIoChannelArea *areas;
+    int frames_left = frame_count_max;
+    int err;
 
-    size_t n = sample_rate * 10;
-    std::vector<int16_t> samples;
-    for (size_t i = 0; i < n; ++i)
-        samples.push_back(sin(i / M_PI) * pow(2, 16) / 2);
+    while (frames_left > 0) {
+        int frame_count = frames_left;
 
-    auto chunk = sf::SoundBuffer();
-    chunk.loadFromSamples(samples.data(), samples.size(), 1, sample_rate);
+        if ((err = soundio_outstream_begin_write(outstream, &areas, &frame_count))) {
+            fprintf(stderr, "%s\n", soundio_strerror(err));
+            exit(1);
+        }
 
-    auto sound = sf::Sound();
+        if (!frame_count)
+            break;
 
-    auto chunk2 = sf::SoundBuffer();
-    chunk2.loadFromFile("/home/clem/Workspace/Telescope/test/ok_desu_ka.wav");
+        float pitch = 440.0f;
+        float radians_per_second = pitch * 2.0f * PI;
+        for (int frame = 0; frame < frame_count; frame += 1) {
+            float sample = sin((seconds_offset + frame * seconds_per_frame) * radians_per_second);
+            for (int channel = 0; channel < layout->channel_count; channel += 1) {
+                float *ptr = (float*)(areas[channel].ptr + areas[channel].step * frame);
+                *ptr = sample;
+            }
+        }
+        seconds_offset = fmod(seconds_offset + seconds_per_frame * frame_count, 1.0);
 
-    auto chunk1 = sf::SoundBuffer();
-    chunk1.loadFromSamples(samples.data(), samples.size(), 1, sample_rate);
-    sound.setBuffer(chunk1);
-    sound.play();
+        if ((err = soundio_outstream_end_write(outstream))) {
+            fprintf(stderr, "%s\n", soundio_strerror(err));
+            exit(1);
+        }
 
-    for (size_t i = 0; i < chunk1.getSampleCount(); ++i)
-    {
-        auto val = chunk1.getSamples()[i];
+        frames_left -= frame_count;
+    }
+}
 
-        if (abs(val) > 16)
-            std::cout << val << std::endl;
+int main(int argc, char **argv) {
+    int err;
+    struct SoundIo *soundio = soundio_create();
+    if (!soundio) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    if ((err = soundio_connect(soundio))) {
+        fprintf(stderr, "error connecting: %s\n", soundio_strerror(err));
+        return 1;
+    }
+
+    soundio_flush_events(soundio);
+
+    int default_out_device_index = soundio_default_output_device_index(soundio);
+    if (default_out_device_index < 0) {
+        fprintf(stderr, "no output device found\n");
+        return 1;
+    }
+
+    struct SoundIoDevice *device = soundio_get_output_device(soundio, default_out_device_index);
+    if (!device) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+
+    fprintf(stderr, "Output device: %s\n", device->name);
+
+    struct SoundIoOutStream *outstream = soundio_outstream_create(device);
+    if (!outstream) {
+        fprintf(stderr, "out of memory\n");
+        return 1;
+    }
+    outstream->format = SoundIoFormatFloat32NE;
+    outstream->write_callback = write_callback;
+
+    if ((err = soundio_outstream_open(outstream))) {
+        fprintf(stderr, "unable to open device: %s", soundio_strerror(err));
+        return 1;
+    }
+
+    if (outstream->layout_error)
+        fprintf(stderr, "unable to set channel layout: %s\n", soundio_strerror(outstream->layout_error));
+
+    if ((err = soundio_outstream_start(outstream))) {
+        fprintf(stderr, "unable to start device: %s\n", soundio_strerror(err));
+        return 1;
+    }
+
+    for (;;)
+        soundio_wait_events(soundio);
+
+    soundio_outstream_destroy(outstream);
+    soundio_device_unref(device);
+    soundio_destroy(soundio);
+    return 0;
 }
